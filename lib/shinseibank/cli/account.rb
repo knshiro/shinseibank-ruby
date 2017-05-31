d5c075d1f179a4fbc92041b6faba60214c3871c1
David Stosik <david.stosik+git-noreply@gmail.com>
2017-05-31 12:02:39 +0900

    Implement user-friendly display of transaction history in CLI

--- lib/shinseibank/cli/account.rb
+++ lib/shinseibank/cli/account.rb
@@ -4,6 +4,8 @@ require "shinseibank/cli/subcommand"
 class ShinseiBank
   module CLI
     class Account < Subcommand
+      FULL_WIDTH_REGEXP = /[^ -~｡-ﾟ]/.freeze
+
       desc "show", "Show your account details"
       def show
         non_empty_accounts.each do |account|
@@ -13,9 +15,14 @@ class ShinseiBank
       end
 
       desc "history [ACCOUNT_ID]", "Display the transaction history of an account (defaults to the first checkings account)"
+      option :from, type: :string, aliases: "-f"
+      option :to, type: :string, aliases: "-t"
       def history(account_id = nil)
-        puts "ID: #{account_id}"
-        puts options
+        puts history_header
+
+        shinsei_bank.get_history(from: from, to: to, id: account_id).each do |tx|
+          puts format_transaction(tx)
+        end
       end
 
       private
@@ -30,6 +37,48 @@ class ShinseiBank
           OpenStruct.new(account).instance_eval do
             "#{description} - #{id} (#{type}):\t#{currency} #{balance}"
           end
+        end
+
+        def history_header
+          format_transaction(
+            date: "Date",
+            ref_no: "Reference",
+            description: "Description",
+            debit: "Debit",
+            credit: "Credit",
+            balance: "Balance"
+          )
+        end
+
+        def format_transaction(transaction)
+          [
+            transaction[:date].to_s.ljust(10),
+            transaction[:ref_no].to_s.ljust(10),
+            pad_zenkaku(transaction[:description], 40),
+            transaction[:debit].to_s.rjust(10),
+            transaction[:credit].to_s.rjust(10),
+            transaction[:balance].to_s.rjust(12),
+          ].join(" | ")
+        end
+
+        def pad_zenkaku(string, to_length)
+          length = string.length + string.scan(FULL_WIDTH_REGEXP).count
+          padding = to_length - length
+          if padding >= 0
+            string + " " * padding
+          else
+            string
+          end
+        end
+
+        def from
+          return unless options[:from]
+          Date.parse(options[:from])
+        end
+
+        def to
+          return unless options[:to]
+          Date.parse(options[:to])
         end
     end
   end
