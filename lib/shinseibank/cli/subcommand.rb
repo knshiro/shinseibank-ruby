fac4fb22068e4ee4ada1047dbdcba4321620dd72
David Stosik <david.stosik+git-noreply@gmail.com>
2017-06-03 17:50:07 +0900

    Allow passing credentials via environment variables

--- lib/shinseibank/cli/subcommand.rb
+++ lib/shinseibank/cli/subcommand.rb
@@ -3,9 +3,9 @@ require "yaml"
 class ShinseiBank
   module CLI
     class Subcommand < Thor
-      DEFAULT_CREDENTIALS_PATH = "./shinsei_account.yaml".freeze
+      DEFAULT_CREDENTIALS_YAML = "./shinsei_account.yaml".freeze
 
-      class_option :credentials, type: :string, aliases: "-c", default: DEFAULT_CREDENTIALS_PATH
+      class_option :credentials, type: :string, aliases: "-c", default: DEFAULT_CREDENTIALS_YAML
 
       private
 
@@ -19,7 +19,25 @@ class ShinseiBank
         end
 
         def credentials
+          env_credentials || yaml_credentials
+        end
+
+        def yaml_credentials
           YAML.load_file(options[:credentials])
+        end
+
+        def env_credentials
+          return unless env_var(:account)
+          {
+            "account" => env_var(:account),
+            "password" => env_var(:password),
+            "pin" => env_var(:pin),
+            "code_card" => env_var(:code_card).split(",")
+          }
+        end
+
+        def env_var(key)
+          ENV["SHINSEIBANK_#{key.upcase}"]
         end
     end
   end
