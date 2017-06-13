3aee97ba2360f304e9d925a9c896bb72f42dc3a3
David Stosik <david.stosik+git-noreply@gmail.com>
2017-06-13 10:24:48 +0900

    Define CLI commands to show transfer history

--- lib/shinseibank/cli.rb
+++ lib/shinseibank/cli.rb
@@ -6,6 +6,7 @@ class ShinseiBank
   module CLI
     class Main < Thor
       register(Account, "account", "account <command>", "Get informations about your account.")
+      register(Transfer, "transfer", "transfer <command>", "Issue transfers to registered accounts.")
       register(Fund, "fund", "fund <command>", "Get informations about your funds.")
     end
   end
